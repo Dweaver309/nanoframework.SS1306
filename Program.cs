ea0445ddcd68960d2b8d29adba690156003fe9bd
David E Weaver <dweaver309@gmail.com>
2018-07-12 19:35:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using SS1306;
+
+namespace SS1306.Driver
+{
+    public class Program
+    {
+        public static void Main()
+        {
+           
+                OLED.Initialize();
+
+                OLED.Write(1, 2, "Hello from ESP32!");
+
+
+                while (true)
+
+                {
+                                               
+                    Thread.Sleep(1000);
+                 
+                }
+            }
+            
+        }
+    }
